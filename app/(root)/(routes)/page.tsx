60ce69bbe86be6e04182725c6223b5346feb8478
Annie David <E20CSE068@bennett.edu.in>
2023-08-13 05:43:56 +0530

    search input done

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 //app/page.tsx
 import { UserButton } from "@clerk/nextjs";
+import SearchInput from "@/components/SearchInput";
 
 const RootPage = () => {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
     </div>
   );
 };
